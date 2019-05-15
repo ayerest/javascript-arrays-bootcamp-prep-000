a4136f28111e0c9a11706f0f11c9b62e8b943d20
Iris S <irisiseve@gmail.com>
2019-05-15 01:47:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,9 +5,14 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
   return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
 }
